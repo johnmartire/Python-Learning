b69a869c430e3bfbeb54b86a7944fcb331f74e76
John Martire <johnmartire818@gmail.com>
2024-02-29 14:02:34 -0500

    created main.c P1

---
+++ C Learning/Project 1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int myFavoriteNumber = 0;
+
+    printf("Please enter your favortite number\n");
+    scanf("%d", &myFavoriteNumber);
+    printf("you entered number: %d\n", myFavoriteNumber);
+
+    return 0;
+    
+}    
