5a816b2bd5ea715e09d4ea99ef632aaa842157c8
John Martire <johnmartire818@gmail.com>
2024-11-17 09:42:58 -0500

    py

---
+++ Project 4/main.py
@@ -0,0 +1,11 @@
+name = "jack"
+print(name)
+
+name="Angela"
+
+# print (len)(input)("What is your name")))
+
+username= input("what is username?")
+lenth= len(username)
+print(lenth)
+
---
+++ Project 5/main.py
@@ -0,0 +1,3 @@
+glass1 = "milk";var=glass2="juice"  
+glass2=glass1;glass1=var
+print(glass1,glass2)
