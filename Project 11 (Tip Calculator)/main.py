fac24143dacf3ad7b6022171b507b0c082050144
John Martire <johnmartire818@gmail.com>
2024-11-19 13:35:42 -0500

    tip cal

--- Project 11 (Tip Calculator)/main.py
+++ Project 11 (Tip Calculator)/main.py
@@ -1,6 +1,11 @@
+
 print("Welcome to the tip calculator!")
-bill=input("What was the total bill? ")
-tip=input("How much tip would you like to give ? 10, 12 , or 15?")
-spilt=input("How many people to spilt the bill? ")
-print(tip/100 * bill)
-print("Each person should pay:" + bill/spilt)
+bill =input("what is the total bill? ")
+tip=input("How much tip would like to give? 10, 12, or 15? ")
+tip= a=10; b=12 ;c=15
+tip= a=1.10;b=1.12;c=1.15
+spilt= input("How many people to spilt the bill?")
+bill = float(bill);tip = float(tip); spilt= float(spilt)
+total=tip*bill/spilt
+total= float(total)
+print("Each person should pay:");print(round(total,2))
