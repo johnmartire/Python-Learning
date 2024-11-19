95ef809cc0d6817e66c9a354b5b21407840122c1
John Martire <johnmartire818@gmail.com>
2024-11-19 15:03:50 -0500

    py

--- Project 11 (Tip Calculator)/main.py
+++ Project 11 (Tip Calculator)/main.py
@@ -1,6 +1,6 @@
 
 print("Welcome to the tip calculator!")
-bill =input("what is the total bill? ")
+bill =input("what is the total bill? $")
 tip=input("How much tip would like to give? 10, 12, or 15? ")
 tip= a=10; b=12 ;c=15
 tip= a=1.10;b=1.12;c=1.15
