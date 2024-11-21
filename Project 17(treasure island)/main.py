8f422d76836fd58f846668ecdd1474c6c2cb0e23
John Martire <johnmartire818@gmail.com>
2024-11-21 17:02:57 -0500

    fdghdf

---
+++ Project 17(treasure island)/main.py
@@ -0,0 +1,47 @@
+print("""
+X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X
+|                           ,,'``````````````',,                            |
+X                        ,'`                   `',                          X
+|                      ,'                         ',                        |
+X                    ,'          ;       ;          ',                      X
+|       (           ;             ;     ;             ;     (               |
+X        )         ;              ;     ;              ;     )              X
+|       (         ;                ;   ;                ;   (               |
+X        )    ;   ;    ,,'```',,,   ; ;   ,,,'```',,    ;   ;               X
+|       (    ; ',;   '`          `',   ,'`          `'   ;,' ;              |
+X        )  ; ;`,`',  _--~~~~--__   ' '   __--~~~~--_  ,'`,'; ;     )       X
+|       (    ; `,' ; :  /       \~~-___-~~/       \  : ; ',' ;     (        |
+X  )     )   )',  ;   -_\  o    /  '   '  \    o  /_-   ;  ,'       )   (   X
+| (     (   (   `;      ~-____--~'       '~--____-~      ;'  )     (     )  |
+X  )     )   )   ;            ,`;,,,   ,,,;',            ;  (       )   (   X
+| (     (   (  .  ;        ,'`  (__ '_' __)  `',        ;  . )     (     )  |
+X  )     \/ ,".). ';    ,'`        ~~ ~~        `',    ;  .(.", \/  )   (   X
+| (   , ,'|// / (/ ,;  '        _--~~-~~--_        '  ;, \)    \|', ,    )  |
+X ,)  , \/ \|  \\,/  ;;       ,; |_| | |_| ;,       ;;  \,//  |/ \/ ,   ,   X
+|",   .| \_ |\/ |#\_/;       ;_| : `~'~' : |_;       ;\_/#| \/| _/ |.   ,"  |
+X#(,'  )  \\\#\ \##/)#;     :  `\/       \/   :     ;#(\##/ /#///  (  ',)# ,X
+|| ) | \ |/ /#/ |#( \; ;     :               ;     ; ;/ )#| \#\ \| / | ( |) |
+X\ |.\\ |\_/#| /#),,`   ;     ;./\_     _/\.;     ;   `,,(#\ |#\_/| //.| / ,X
+| \\_/# |#\##/,,'`       ;     ~~--|~|~|--~~     ;       `',,\##/#| #\_// \/|
+X  ##/#  #,,'`            ;        ~~~~~        ;            `',,#  #\##  //X
+|####@,,'`                 `',               ,'`                 `',,@####| |
+X#,,'`                        `',         ,'`                        `',,###X
+|'  spb                          ~~-----~~                               `' |
+X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X
+""")
+
+print("Welcome to escape the Demon!")
+print("Your objective is to find the key and escape!")
+direction=input("You are running in a mansion you see a an elevator that goes down and stairs that go up. Where do you go? type UP or DOWN ")
+if direction == "UP":
+   left = input("You have heard a loud scream saying YOU CANNOT LEAVE do you break the window and jump or take a left to a ladder? type BREAK or LEFT ")
+   if left == "LEFT":
+       direction3= input("you go up the ladder and see a open golden box with a key. you fall through the floor and see a locked golden door and a locked black door? Which do you open?type GOLDEN or BLACK ")
+       if direction3 == "GOLDEN":
+        print("You've open the golden door and are out side a very tall building and find a parachute. You put the parachute on jump and glide to saftey... MISSON COMPLETE YOU SURVIVED!")
+        if "BREAK":
+         print("you break the window and jump out the window falling to your death.... GAME OVER")
+elif direction == "DOWN":
+     print("You enter the elevator it goes down and opens into a room full of fire and the demon impales you.... GAME OVER")
+else:
+          print("You've opened the black door enter a pitch black room and fall into fire... the demon grabs you and crushes your skull.. GAME OVER")
