6242cbac55fab76b819333a2e757affb17529dd1
John Martire <johnmartire818@gmail.com>
2025-03-25 20:49:38 -0400

    Todays work 3/25

---
+++ Project 37/main.py
@@ -0,0 +1,163 @@
+from turtle import Turtle,Screen
+import random
+tim=Turtle()
+tim.shape("arrow")
+tim.color("red")
+
+def triangle():
+    tim.color("red")
+    tim.right(120)
+    tim.forward(100)
+    tim.right(120)
+    tim.forward(100)
+    tim.right(120)
+    tim.forward(100)
+    
+def square():
+    tim.color("yellow")
+    tim.right(90)
+    tim.forward(100)
+    tim.right(90)
+    tim.forward(100)
+    tim.right(90)
+    tim.forward(100)
+    tim.right(90)
+    tim.forward(100)
+def pentagon():
+    tim.color("orange")
+    tim.right(72)
+    tim.forward(100)
+    tim.right(72)
+    tim.forward(100)
+    tim.right(72)
+    tim.forward(100)
+    tim.right(72)
+    tim.forward(100)
+    tim.right(72)
+    tim.forward(100)
+def hex():
+    tim.color("medium orchid")
+    tim.right(60)
+    tim.forward(100)
+    tim.right(60)
+    tim.forward(100)
+    tim.right(60)
+    tim.forward(100)
+    tim.right(60)
+    tim.forward(100)
+    tim.right(60)
+    tim.forward(100)
+def hep():
+    tim.color("indigo")
+    tim.right(60)
+    tim.forward(100)
+    tim.right(51.4285714286)
+    tim.forward(100)
+    tim.right(51.4285714286)
+    tim.forward(100)
+    tim.right(51.4285714286)
+    tim.forward(100)
+    tim.right(51.4285714286)
+    tim.forward(100)
+    tim.right(51.4285714286)
+    tim.forward(100)
+    tim.right(51.4285714286)
+    tim.forward(100)
+    tim.right(51.4285714286)
+    tim.forward(100)
+
+def oct():
+    tim.color("crimson")
+    tim.right(45)
+    tim.forward(100)
+    tim.right(45)
+    tim.forward(100)
+    tim.right(45)
+    tim.forward(100)
+    tim.right(45)
+    tim.forward(100)
+    tim.right(45)
+    tim.forward(100)
+    tim.right(45)
+    tim.forward(100)
+    tim.right(45)
+    tim.forward(100)
+    tim.right(45)
+    tim.forward(100)
+def non():
+    tim.color("blue")
+    tim.right(40)
+    tim.forward(100)
+    tim.right(40)
+    tim.forward(100)
+    tim.right(40)
+    tim.forward(100)
+    tim.right(40)
+    tim.forward(100)
+    tim.right(40)
+    tim.forward(100)
+    tim.right(40)
+    tim.forward(100)
+    tim.right(40)
+    tim.forward(100)
+    tim.right(40)
+    tim.forward(100)
+    tim.right(40)
+    tim.forward(100)
+def dec():
+    tim.color("green")
+    tim.right(36)
+    tim.forward(100)
+    tim.right(36)
+    tim.forward(100)
+    tim.right(36)
+    tim.forward(100)
+    tim.right(36)
+    tim.forward(100)
+    tim.right(36)
+    tim.forward(100)
+    tim.right(36)
+    tim.forward(100)
+    tim.right(36)
+    tim.forward(100)
+    tim.right(36)
+    tim.forward(100)
+    tim.right(36)
+    tim.forward(100)
+    tim.right(36)
+    tim.forward(100)
+
+
+triangle()
+square()
+pentagon()
+hex()
+hep()
+oct()
+non()
+dec()
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
---
+++ Project 37/random_walk.py
@@ -0,0 +1,12 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+for _ in range(1000):
+    tim.forward()
+    tim.right()
+    tim.left()
